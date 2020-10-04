08e5ef1bbb4dcb311aa84cc7d18f48e067ae222e
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2020-10-04 18:58:26 +0530

    Update autoban.py

--- pyrobot/plugins/autoban.py
+++ pyrobot/plugins/autoban.py
@@ -14,10 +14,10 @@ async def get_ban_command(message):
                   user_id=member.id,
                   until_date=until_date_val
               )
-          except Exception as error:
-              await message.reply_text(
-                  str(error)
-              )
+        except Exception as error:
+            await message.reply_text(
+                str(error)
+            )
 @PyroBot.on_message(filters.chat(chats=AUTH_CHANNEL) & filters.new_chat_members)
 async def auto_ban(_, message):
        await get_ban_command(message)
