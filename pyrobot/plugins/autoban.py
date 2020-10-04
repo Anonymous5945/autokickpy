39659e8314b2bb50a233a15030a75d48d513dcf7
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2020-10-04 18:55:16 +0530

    Update autoban.py

--- pyrobot/plugins/autoban.py
+++ pyrobot/plugins/autoban.py
@@ -9,14 +9,15 @@ async def get_ban_command(message):
     until_date_val = int(time.time() + 40)
     for member in message.new_chat_members:
         try:
-            await message.chat.kick_member(
-                user_id=member.id,
-                until_date=until_date_val
-            )
-        except Exception as error:
-            await message.reply_text(
-                str(error)
-            )
+            if member.id >= 1300000000
+              await message.chat.kick_member(
+                  user_id=member.id,
+                  until_date=until_date_val
+              )
+          except Exception as error:
+              await message.reply_text(
+                  str(error)
+              )
 @PyroBot.on_message(filters.chat(chats=AUTH_CHANNEL) & filters.new_chat_members)
 async def auto_ban(_, message):
        await get_ban_command(message)
