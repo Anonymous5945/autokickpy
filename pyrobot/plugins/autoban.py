1efe028cc4d42efc7750e1ae856dfdd467b501d1
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2020-10-04 15:54:47 +0530

    Update autoban.py

--- pyrobot/plugins/autoban.py
+++ pyrobot/plugins/autoban.py
@@ -1,12 +1,10 @@
 from pyrogram import Client, filters
 from pyrobot import COMMAND_HAND_LER
-from pyrobot.helper_functions.string_handling import extract_time
 
 from pyrobot.pyrobot import PyroBot
 
 async def get_ban_command(message):
-    n="3Os"
-    until_date_val = extract_time(n)
+    until_date_val = 1601807059
     for member in message.new_chat_members:
         try:
             await message.chat.kick_member(
