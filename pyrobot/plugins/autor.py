f4edb00aeb9698d9e7617fc43c929b070c2fd6fd
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2021-02-06 11:28:11 +0530

    Create autor.py

---
+++ pyrobot/plugins/autor.py
@@ -0,0 +1,21 @@
+import time
+from pyrogram import Client, filters
+
+from pyrobot.pyrobot import PyroBot
+
+from pyrobot import AUTH_CHANNEL
+
+
+async def get_banr_command(message):
+   for member in message.left_chat_member:
+        try:
+              await message.chat.kick_member(
+                  user_id=member.id
+              )
+        except Exception as error:
+            await message.reply_text(
+                str(error)
+            )
+@PyroBot.on_message(filters.chat(chats=AUTH_CHANNEL) & filters.left_chat_member)
+async def autor_ban(_, message):
+       await get_banr_command(message)
