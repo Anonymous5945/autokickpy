63d8d03f86284dfeef677cd926d9e9b339f98a88
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2021-02-06 11:34:55 +0530

    Update autoforward.py

--- pyrobot/plugins/autoforward.py
+++ pyrobot/plugins/autoforward.py
@@ -1,27 +1,25 @@
 
-import time
-from pyrogram import Client, filters
+#import time
+#from pyrogram import Client, filters
 
-from pyrobot.pyrobot import PyroBot
+#from pyrobot.pyrobot import PyroBot
 
-from pyrobot import AUTH_CHANNEL, LIMIT_ID 
-
-
-async def get_forward_command(message):
-   for m in message:
-        try:
-            await client.forward_messages(
-                  chat_id=-1001428281865,
-                  from_chat_id=m.chat.id,
-                  message_ids=m.message_id
-              )
-            await asyncio.sleep(3)
-        except Exception as error:
-            await message.reply_text(
-                str(error)
-            )
+#from pyrobot import AUTH_CHANNEL, LIMIT_ID 
 
 
+#async def get_forward_command(message):
+#   for m in message.incoming:
+  #      try:
+       #     await client.forward_messages(
+         #         chat_id=-1001428281865,
+      #            from_chat_id=m.chat.id,
+     #             message_ids=m.message_id
+      #        )
+    #        await asyncio.sleep(3)
+     #   except Exception as error:
+       #     await message.reply_text(
+        #        str(error)
+     #       )
 
 
 
@@ -35,6 +33,8 @@ async def get_forward_command(message):
 
 
 
-@PyroBot.on_message(filters.chat(chats=AUTH_CHANNEL) & filters.incoming)
-async def auto_forward(_, message):
-       await get_forward_command(message)
+
+
+#@PyroBot.on_message(filters.chat(chats=AUTH_CHANNEL) & filters.incoming)
+#async def auto_forward(_, message):
+#       await get_forward_command(message)
